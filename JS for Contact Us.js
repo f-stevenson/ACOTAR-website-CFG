3132ec5f71f0933c5fe48ee4faed2e85e3407199
f-stevenson <134550525+f-stevenson@users.noreply.github.com>
2023-07-24 10:34:04 +0100

    Add files via upload

---
+++ JS for Contact Us.js
@@ -0,0 +1,7 @@
+function pressAlert() {
+    alert("Thank you for your message! The Suriel will get back to you as soon as possible.");
+  }
+
+  const button = document.getElementById("submit");
+
+  button.addEventListener("click", pressAlert);
